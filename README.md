bcebf1e770e462435f31a1ac1c26c0d09b60f8fc
tanjabrzak <brzak.tanja@gmail.com>
2015-08-30 14:34:23 +0200

    readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # angular-InfiniteScroll 
+Example of simple AngularJS directive for infinite scroll attached to an element, and some base routing
--- scroll.js
+++ scroll.js
@@ -34,9 +34,9 @@ app.directive('whenScrolled', function() {
 });
 
 app.controller('scrollCtrl', function($scope) {
-	
-	$scope.toggle = {showClickMe: true};
-	
+
+    $scope.toggle = {showClickMe: true};
+
     $scope.items = [];
     
     var counter = 0;
